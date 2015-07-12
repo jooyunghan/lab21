856309edb8d27373371653b571776db80c0b068f
Nicolas Rolland <nicolas.rolland@gmail.com>
2015-07-12 19:31:08 +0200

    with StateT transformer

--- src/Main.hs
+++ src/Main.hs
@@ -2,6 +2,8 @@
 module Main where
 
 import Control.Monad.State
+import qualified Data.Map.Lazy as M
+import qualified MonadStatePlusWithClass as V2
 
 data Trace = Trace { bindCount :: Int , displayCount :: Int , returnCount :: Int }    
 newtype StateMonadPlus s a =  StateMonadPlus { inner:: State (s, (Int, Int, Int)) a }
@@ -45,14 +47,25 @@ annotate :: String -> StateMonadPlus s a -> StateMonadPlus s a
 annotate = undefined
 
 exemple1 :: StateMonadPlus s String
-exemple1 = do return (3::Int)  >> return (4::Int)
-              return (5::Int)
+exemple1 = do _<- return (3::Int)  >> return (4::Int)
+              _<- return (5::Int)
               diagnostics
 
-           
+
+exemple3 :: (MonadState s m) => m ()
+exemple3 =
+    do _ <- return (3::Int)  >> return (4::Int)
+       _ <- return (5::Int)
+       return ()
+            
 main :: IO ()
 main =
     do
-      let (a,s) = runStateMP  exemple1 ("",(0,0,0))
-      putStrLn (show a)
+      let (_,t) = runStateMP  exemple3 ("",(0,0,0))
+      putStrLn ("first version : " ++ show t )
+      -- we can see everything if I dont restrict the exported functions
+      -- from MonadStatePlusWithClass
+      let (a,s) =  evalState  (V2.runMP exemple3 M.empty)  (0::Integer)
+      putStrLn ("second version : " ++ show s )
+      
   
