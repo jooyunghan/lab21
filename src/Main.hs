829f308ea4cf9580aa1314287255c6f39d921a50
Jooyung Han <jooyung.han@gmail.com>
2015-07-13 16:05:36 +0200

    Done annotate

--- src/Main.hs
+++ src/Main.hs
@@ -1,40 +1,71 @@
 {-# LANGUAGE FlexibleInstances, MultiParamTypeClasses  #-}
 module Main where
 
+import Control.Applicative
 import Control.Monad.State 
+import Control.Monad 
 
-newtype StateMonadPlus s a =  StateMonadPlus { inner:: State (s, (Int, Int, Int)) a }
+data Diag = Diag {
+  label:: Maybe String,
+  n_returns:: Int,
+  n_binds:: Int,
+  n_diagnostics:: Int
+} deriving (Eq, Show)
 
-stateMP :: ((s, (Int, Int, Int)) -> (a, (s, (Int, Int, Int)))) -> StateMonadPlus s a
-stateMP f = StateMonadPlus(state f)
+emptyDiag = Diag Nothing 0 0 0
 
-runStateMP :: StateMonadPlus s a -> (s,(Int,Int,Int)) -> (a, (s, (Int, Int, Int)))
-runStateMP smp = runState (inner smp)
+addDiagnostics d@Diag{n_diagnostics=n} = d{n_diagnostics=n+1}
 
-instance Monad (StateMonadPlus s) where
-    return e =  stateMP (\(s, (a,b,ret))->(e, (s, (a,b,ret+1))) )
-    m >>= f  =  stateMP (\innerS ->  let (res, (s,(a,b,ret))) = runStateMP m innerS
-                                   in  runStateMP (f res) (s,(a+1, b, ret)))
+addReturn d@Diag{n_returns=n} = d{n_returns=n+1}
 
-instance MonadState s (StateMonadPlus s) where
-    get = undefined
-    put = undefined
+addBind d@Diag{n_binds=n} = d{n_binds=n+1}
 
-          
+setLabel :: String -> Diag -> Diag
+setLabel s d = d{label=Just s}
+
+data SMP m a = SMP { runSMP:: StateT Diag m a }
+
+instance (Monad m) => Functor (SMP m) where
+  fmap = liftM 
+
+instance (Monad m) => Applicative (SMP m) where
+  pure = return
+  (<*>) = ap
+
+instance (Monad m) => Monad (SMP m) where
+  return x = SMP $ do
+    modify addReturn
+    return x
+  m >>= f = SMP $ do
+    modify addBind
+    a <- runSMP m
+    runSMP (f a)
+
+type StateMonadPlus s a = SMP (State s) a
+
 diagnostics :: StateMonadPlus s String
-diagnostics =  stateMP (\(s, (a,b,ret)) -> let newCounts = (a,b+1, ret) in (show newCounts, (s, newCounts))) 
+diagnostics = SMP $ StateT $ \diag -> 
+  let diag' = addDiagnostics diag
+  in state(\s -> ((show diag', diag'),s)) 
 
-              
 annotate :: String -> StateMonadPlus s a -> StateMonadPlus s a
-annotate = undefined
+annotate s smp = SMP $ do
+  modify (setLabel s)
+  runSMP smp
 
-exemple1 :: StateMonadPlus s String
-exemple1 = do return 3  >> return 4
+example1 :: StateMonadPlus s String
+example1 = do return 3  >> return 4
               return 5
               diagnostics
-           
+
+example2 :: StateMonadPlus s String
+example2 = do annotate "A" (return 3 >> return 4)
+              return 5
+              diagnostics
+
 main :: IO ()
-main =
-    do
-      let (a,s) = runStateMP  exemple1 ("",(0,0,0))
-      putStrLn (show a)
+main = do
+      let diag1 = evalState (execStateT (runSMP example1) emptyDiag) 0
+      putStrLn ("example1 " ++ show diag1)
+      let diag2 = evalState (execStateT (runSMP example2) emptyDiag) 0
+      putStrLn ("example2 " ++ show diag2)
